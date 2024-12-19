6df48de7061a021255ba596f5df3de10ae97ef53
Alex Youssef <alex.youssef@live.com>
2024-12-19 10:12:55 +0200

    first commit

---
+++ src/main/java/PromptEngineeringApplication.java
@@ -0,0 +1,11 @@
+package se.alex.lexicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptEngineeringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PromptEngineeringApplication.class, args);
+    }
+}
